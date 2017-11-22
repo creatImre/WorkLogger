7405f9295a28402611bdc0b7f7a2b9549b41649e
creatImre <imre.torma93@gmail.com>
2017-11-22 15:54:52 +0100

    added missing annotations, fixed bad GET to PUT

--- app/src/main/java/com/hw/szoftarch/worklogger/networking/WorkLoggerService.java
+++ app/src/main/java/com/hw/szoftarch/worklogger/networking/WorkLoggerService.java
@@ -2,11 +2,11 @@ package com.hw.szoftarch.worklogger.networking;
 
 import com.hw.szoftarch.worklogger.entities.Project;
 import com.hw.szoftarch.worklogger.entities.User;
-import com.hw.szoftarch.worklogger.entities.UserLevel;
 import com.hw.szoftarch.worklogger.entities.WorkingHour;
 
 import io.realm.RealmList;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -18,20 +18,20 @@ public interface WorkLoggerService {
     @GET("auth/")
     Call<User> login();
 
-    @GET("auth/{googleId}")
-    Call<String> setUserLevel(@Path("googleId") String googleId, UserLevel level);
+    @PUT("auth/{googleId}")
+    Call<String> setUserLevel(@Path("googleId") String googleId, @Body String level);
 
     @GET("report/{projectName}")
     Call<Project> getReportByProjectName(@Path("projectName") String projectName);
 
     @POST("hour/")
-    Call<String> addWorkingHour(WorkingHour workingHour);
+    Call<String> addWorkingHour(@Body WorkingHour workingHour);
 
     @PUT("hour/")
-    Call<String> updateWorkingHour(WorkingHour workingHour);
+    Call<String> updateWorkingHour(@Body WorkingHour workingHour);
 
     @DELETE("hour/")
-    Call<String> removeWorkingHour(WorkingHour workingHour);
+    Call<String> removeWorkingHour(@Body WorkingHour workingHour);
 
     @GET("hour/")
     Call<RealmList<WorkingHour>> getWorkingHoursByUser();
