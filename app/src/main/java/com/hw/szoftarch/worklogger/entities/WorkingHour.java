bdb45ad1470a7bd85c7275cb99ffb24b1f4418a1
creatImre <imre.torma93@gmail.com>
2017-11-24 18:20:07 +0100

    updated comparator

--- app/src/main/java/com/hw/szoftarch/worklogger/entities/WorkingHour.java
+++ app/src/main/java/com/hw/szoftarch/worklogger/entities/WorkingHour.java
@@ -9,15 +9,15 @@ import java.util.Date;
 
 public class WorkingHour {
 
-    public static class WorkingHourDateComparator implements Comparator<WorkingHour> {
-        private static WorkingHourDateComparator mInstance;
+    public static class WorkingHourComparator implements Comparator<WorkingHour> {
+        private static WorkingHourComparator mInstance;
 
-        private WorkingHourDateComparator() {
+        private WorkingHourComparator() {
         }
 
-        public static WorkingHourDateComparator getInstance() {
+        public static WorkingHourComparator getInstance() {
             if (mInstance == null) {
-                mInstance = new WorkingHourDateComparator();
+                mInstance = new WorkingHourComparator();
             }
             return mInstance;
         }
@@ -26,7 +26,22 @@ public class WorkingHour {
         public int compare(WorkingHour workingHour1, WorkingHour workingHour2) {
             final Long startDate1 = workingHour1.getStarting();
             final Long startDate2 = workingHour2.getStarting();
-            return startDate1.compareTo(startDate2);
+
+            final int dateCompareResult = startDate1.compareTo(startDate2);
+            if (dateCompareResult != 0) {
+                return dateCompareResult;
+            }
+
+            final Long issueId1 = workingHour1.getIssue().getId();
+            final Long issueId2 = workingHour2.getIssue().getId();
+            final int issueCompareResult = issueId1.compareTo(issueId2);
+            if (issueCompareResult != 0) {
+                return issueCompareResult;
+            }
+
+            final Long duration1 = workingHour1.getDuration();
+            final Long duration2 = workingHour2.getDuration();
+            return duration1.compareTo(duration2);
         }
     }
 
